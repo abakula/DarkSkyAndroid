a7f22e96f8cfbaa7e0a52f63193676a082c11a76
Adrian Bakula <abakula@shopwiki.com>
2013-10-16 10:51:08 -0400

    Add initial Daydream service to the DarkSky App

---
+++ src/com/adrian/darksky/android/DarkSkyDaydream.java
@@ -0,0 +1,18 @@
+package com.adrian.darksky.android;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.service.dreams.DreamService;
+
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+public class DarkSkyDaydream extends DreamService {
+	
+	@Override
+	public void onDreamingStarted() {
+		super.onDreamingStarted();
+		
+		
+	}
+	
+	
+}
